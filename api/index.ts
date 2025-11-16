34debf86caf2603f02374b0582e4e85b3cd28319
Raj Shah <rajshah9305@gmail.com>
2025-11-15 20:24:13 -0700

    Remove conflicting api/index.ts

--- api/index.ts
+++
@@ -1,4 +0,0 @@
-// This file re-exports the server index for compatibility
-// with deployment platforms that expect an api/index.ts file
-import "dotenv/config";
-export * from "../server/index.js";
